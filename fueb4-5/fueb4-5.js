7622423266ff35b52bd654d3bb3f6972a3485170
Louis Hermier <louis.hermier1@etu.univ-lorraine.fr>
2020-01-16 15:16:55 +0100

    Update fueb4-5.js

--- fueb4-5/fueb4-5.js
+++ fueb4-5/fueb4-5.js
@@ -16,5 +16,7 @@ window.main = function(...argv) {
 	terminal.printl(lectures.map(query => `${query["name"]} hat ${query["credit"]} Credits.`)); // map function: for all elements of the table, will create a string with the name and the number of credits. This is all places in an array, that is displayed
 	// NOT A PART OF THE THING
 	let compactor = (accumulator, currentValue) => accumulator + `${currentValue["name"]}\t${currentValue["type"]}\n`;
+	let addUp = (acc, val) => acc + val.credit;
 	terminal.printl(lectures.reduce(compactor, "")); // uses reduce to compact the array into a string. the second argument of reduce is the initial value of the accumulator. it is here "" because the goal is to get a string
+	terminal.printl(lectures.reduce(addUp, 0)); // uses reduce to make a sum of the credits of all lectures
 };
