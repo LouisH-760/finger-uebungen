019aff67dc7802938612ac9e7dc765a46b555f52
Louis Hermier <louis.hermier1@etu.univ-lorraine.fr>
2020-01-16 14:53:35 +0100

    add Array.reduce use to fueb4-5

--- fueb4-5/fueb4-5.js
+++ fueb4-5/fueb4-5.js
@@ -7,11 +7,13 @@ import {terminal} from "./dfhi.js";
 
 window.main = function(...argv) {
 	let lectures = [
-		{"name": "Informatik 1",
+		{"name": "Inf 1",
 			"credit": 5, "type": "Pflichtfach"},
-		{"name": "Programmierung 1", "credit": 5, "type": "Pflichtfach"},
-		{"name": "Englisch 1",
+		{"name": "Prog 1", "credit": 5, "type": "Pflichtfach"},
+		{"name": "Eng 1",
 			"credit": 2, "type": "Wahlfach"}
 	];
 	terminal.printl(lectures.map(query => `${query["name"]} hat ${query["credit"]} Credits.`)); // map function: for all elements of the table, will create a string with the name and the number of credits. This is all places in an array, that is displayed
+	// NOT A PART OF THE THING
+	terminal.printl(lectures.reduce((accumulator, currentValue) => accumulator + `${currentValue["name"]}\t${currentValue["type"]}\n`, "")); // uses reduce to compact the array into a string. the second argument of reduce is the initial value of the accumulator. it is here "" because the goal is to get a string
 };
