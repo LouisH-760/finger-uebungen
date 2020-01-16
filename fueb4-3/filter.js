cb668a91d38692aebeb29972102b1507f9d7f20f
Louis Hermier <louis.hermier1@etu.univ-lorraine.fr>
2020-01-16 14:19:56 +0100

    Add fueb4-3

---
+++ fueb4-3/filter.js
@@ -0,0 +1,23 @@
+/*
+ * filter.js
+ *
+ */
+
+import {terminal} from "./dfhi.js";
+
+window.main = function(...argv) {
+	let data = [
+		{"name": "Informatik 1",
+			"credit": 5, "type": "Pflichtfach"},
+		{"name": "Programmierung 1", "credit": 5, "type": "Pflichtfach"},
+		{"name": "Englisch 1",
+			"credit": 2, "type": "Wahlfach"},
+		{"name": "Programmierung 3",
+			"credit":4, "type": "Pflichtfach"}
+	];
+	let l1 = data.filter(query => query["name"].charAt(query["name"].length - 1) == '1');
+	let l2 = data.filter(query => query["credit"] % 2 == 0);
+	terminal.printl(l1);
+	terminal.printh("<hr />");
+	terminal.printl(l2);
+};
